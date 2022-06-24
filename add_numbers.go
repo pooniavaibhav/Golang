4860b12f7366e3af3a16db71f39123541b4221b7
webhav <46425617+pooniavaibhav@users.noreply.github.com>
2022-06-24 15:55:34 +0530

    Create add_numbers.go

---
+++ add_numbers.go
@@ -0,0 +1,14 @@
+//need to package our code.
+package main
+
+import "fmt"
+
+//format package for printing.
+
+//main function to tell the beginning of the code.
+func main() {
+	var num int
+	var num2 int = 5
+	num = 4
+	fmt.Print(num + num2)
+}
