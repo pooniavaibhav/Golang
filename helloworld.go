14c30088370c328d946565bddd2fd2d1ce06a97d
webhav <46425617+pooniavaibhav@users.noreply.github.com>
2022-06-24 15:43:10 +0530

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,10 @@
+//need to package our code.
 package main
 
+//format package for printing.
 import "fmt"
 
+//main function to tell the beginning of the code.
 func main() {
 	fmt.Print("Hello World")
 }
